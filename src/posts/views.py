05090b8e65dea9b1a8af0093e3fb1ab33fb271ba
kisumutalent <homiemusa@gmail.com>
2020-04-16 14:25:53 +0300

    Create post form

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+# from .models import Post 
+from .forms import PostForms
 
 def home(request):
 	return render(request, "home.html")
 
 
 def post(request):
+	form = PostForms(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "Successfully Created")
+		# return HttpResponseRedirect(instance.get_obsolute_url())
+	content = {
+		"form": form
+	}
 
-	return render (request, "post.html")
+	return render (request, "post.html", content)
 
 def show(request):
 	return render(request, "show.html")
