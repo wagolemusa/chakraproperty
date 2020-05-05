0b15113a881776ebd5bb484dc8cca4a9e5109246
kisumutalent <homiemusa@gmail.com>
2020-05-05 09:00:31 +0300

    fix error

--- chakra/settings.py
+++ chakra/settings.py
@@ -149,8 +149,8 @@ STATICFILES_DIRS = [
     # '/var/www/static/',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
